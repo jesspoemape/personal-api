80bb2c0e50696e5439091a0a4fcc9a44fabed4a5
Jess Poemape <jesspoemape@gmail.com>
2017-06-28 13:06:45 -0600

    user object filled out

--- user.js
+++ user.js
@@ -1,2 +1,78 @@
-var user = {};
+var user = {
+    name: 'Jess',
+  location: 'Utah, USA',
+  occupations: ['wife', 'student', 'sister', 'daughter'],
+  hobbies: [
+    {
+      name: 'cooking',
+      type: 'home'
+    },
+    {
+      name: 'sewing',
+      type: 'home'
+    },
+    {
+      name: 'reading',
+      type: 'liesure'
+    }
+  ],
+  family: [
+    {
+      name: 'JJ',
+      relation: 'spouse',
+      gender: 'male'
+    },
+    {
+      name: 'Romina',
+      relation: 'sister',
+      gender: 'female'
+    },{
+      name: 'Barista',
+      relation: 'sister',
+      gender: 'female'
+    },{
+      name: 'Stefan',
+      relation: 'sister',
+      gender: 'female'
+    }
+  ],
+  pets: [
+      {
+          name: 'Isaac',
+          breed: 'Long Hair Cat',
+          age: 2.5,
+      },
+      {
+          name: 'Squinch',
+          breed: 'Chihuahua/ Terrier',
+          age: 4
+      },
+      {
+          name: 'Kishka',
+          breed: 'German Shepherd/ Timber Wolf',
+          age: 3
+      },
+      {
+          name: 'Prince',
+          breed: 'Chihuahua',
+          age: 2
+      }
+  ],
+  restaurants: [
+    {
+      name: 'Tacos Don Joaquin',
+      type: 'Mexican',
+      rating: 5
+    },
+    {
+      name: 'Sushi Ya',
+      type: 'Japanese',
+      rating: 5
+    },
+    {
+      name: 'Texas Roadhouse',
+      type: 'American',
+      rating: 4
+    }
+  ]};
 module.exports = user;
