9a0791a35ff3f19868ce4f9af7f52b069998cf18
Jess Poemape <jesspoemape@gmail.com>
2017-06-28 13:11:34 -0600

    middleware set up

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const middleware = require('./controllers/middleware.js');
+const mainCtrl = require('./controllers/mainCtrl');
+
 const app = express();
 
 app.use(bodyParser.json());
